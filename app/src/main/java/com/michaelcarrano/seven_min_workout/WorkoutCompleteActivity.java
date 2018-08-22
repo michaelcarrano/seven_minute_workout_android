cab12e4d4acc5d1c09d7071624a481dd072a81f0
Mitchell Hartweg <mhartweg@student.neumont.edu>
2018-08-22 14:37:01 -0600

    starting workout complete functionality

---
+++ app/src/main/java/com/michaelcarrano/seven_min_workout/WorkoutCompleteActivity.java
@@ -0,0 +1,62 @@
+package com.michaelcarrano.seven_min_workout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WorkoutCompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_complete);
+    }
+
+    public void compleBtnOnClick(View view){
+        //Time
+        //Completed last time
+        //total completed
+        //complete percentage
+
+        //Reps
+        //reps last time
+        //total reps
+        //average
+        //best
+
+        //0 Jumping jacks
+
+
+        //1 wall sit
+
+
+        //2 push up
+
+
+        //3 crunches
+
+
+        //4 step up
+
+
+        //5 squat
+
+
+        //6 tricep dips
+
+
+        //7 plank
+
+
+        //8 high knees
+
+
+        //9 lunges
+
+
+        //10 push up rotation
+
+
+        //11 side planks
+    }
+}
